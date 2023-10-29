353cf5891cf70af315bf297cea0178db91ce5ccd
SabareesanArjunasamy <sabareesan833@gmail.com>
2023-10-29 23:33:24 +0530

    fix: theme switcher refinement

--- src/app/ThemeSwitcher.js
+++ src/app/ThemeSwitcher.js
@@ -2,24 +2,66 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
+const LightIcon = () => (
+  <svg
+    className="h-6 w-6 text-gray-400"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+    />
+  </svg>
+);
+
+const DarkIcon = () => (
+  <svg
+    className="h-6 w-6 text-gray-500"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+    />
+  </svg>
+);
+
 const ThemeSwitcher = () => {
-  const {theme, setTheme } = useTheme();
-  
+  const { theme, setTheme } = useTheme();
+  const [initialThemeSet, setInitialThemeSet] = useState(false);
+
   useEffect(() => {
-    const currentHour = new Date().getHours();
-    console.log('current hour --->  :',currentHour);
-    // Assuming light mode from 7 AM to 7 PM, and dark mode from 7 PM to 7 AM
-    if (currentHour >= 7 && currentHour < 19) {
-      setTheme("light");
+    const storedTheme = sessionStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
     } else {
-      setTheme("dark");
+      const currentHour = new Date().getHours();
+      const newTheme = currentHour >= 7 && currentHour < 19 ? "light" : "dark";
+      setTheme(newTheme);
+      sessionStorage.setItem("theme", newTheme);
+      setInitialThemeSet(true);
     }
-    console.log('theme set  --->  :',theme);
-  }, [theme, setTheme]);
-  
+  }, [setTheme]);
+
+  function toggleTheme() {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    sessionStorage.setItem("theme", newTheme);
+  }
+
   return (
     <div>
-      <button onClick={() => theme === "dark"? setTheme('light'): setTheme("dark")}>Theme</button>
+      <button onClick={toggleTheme}>
+        {theme === "light" ? <DarkIcon /> : <LightIcon />}
+      </button>
     </div>
   );
 };
