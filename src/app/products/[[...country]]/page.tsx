5d2e42a7dad7148cd96cb880c8ebc80fe3ff1543
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2023-11-04 21:07:59 +0530

    WIP Add Dark Mode Toggle for products

--- src/app/products/[[...country]]/page.tsx
+++ src/app/products/[[...country]]/page.tsx
@@ -5,12 +5,15 @@ import ImageSlider from '../../../components/ImageSlider'
 import ProductList from '../../../components/ProductList'
 import Product from '../../../interfaces/Product'
 import Category from '../../../interfaces/ProductCategory'
+import Navbar from '../../../components/ui/navbar/navbar1'
+import Footer from '../../../components/ui/footer/footer'
+import Image from 'next/image'
 
 const ProductsPromo = () => {
   return (
-    <div className="bg-[#F2F5FD] w-full px-12 pt-8 rounded-md flex justify-center align-middle">
+    <div className="bg-[#F2F5FD] dark:bg-secondary-dark dark:text-primary-light w-full px-12 pt-8 rounded-3xl flex justify-center align-middle">
       <div className="w-2/3 flex-1">
-        <h1 className="font-bold">Products</h1>
+        <h1 className="font-bold pb-4 text-xl">Products</h1>
         <p>
           Our Marine Spare & equipment department supply Genuine and OEM spares
           worldwide to Ship Owners, Ship Managers, Repairing Companies, Ship
@@ -38,7 +41,9 @@ const Categories: React.FC<{
   const { name, thumbnailUrl, flag, code } = category
   return (
     <div className="h-full w-full hover:scale-[101%] hover:shadow-lg shadow-sm transition-all transform-gpu ease-in-out duration-200 relative hs-[20.8125rem] ws-[27.375rem]  rounded-[0.5rem] overflow-hidden drop-shadow-md  bg-black">
-      <img
+      <Image
+        width={1000}
+        height={1000}
         src={thumbnailUrl}
         alt={name}
         className="w-full h-full object-cover opacity-70"
@@ -49,7 +54,9 @@ const Categories: React.FC<{
         className="absolute flex justify-center  gap-1 text-center align-baseline text-xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       >
         <div className="w-8 rounded-full overflow-hidden align-middle justify-center">
-          <img
+          <Image
+            width={1000}
+            height={1000}
             className="w-full object-cover h-full"
             src={flag.svg}
             alt={flag.alt}
@@ -91,7 +98,7 @@ const ProductCategories = ({ countryMeta, handleCodeChange }) => {
       )
       getProducts({ code: countryMeta.code }).then(setProducts)
     }
-  }, [countryMeta?.code])
+  }, [countryMeta, setSelectedCategory, categories])
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -105,8 +112,11 @@ const ProductCategories = ({ countryMeta, handleCodeChange }) => {
         {!categories
           ? Array(4)
               .fill(0)
-              .map(() => (
-                <div className="border shadow rounded-md px-4 py-4 h-40 w-full mx-auto">
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className="border shadow rounded-md px-4 py-4 h-40 w-full mx-auto"
+                >
                   <div className="animate-pulse flex space-x-4 ">
                     <div className="rounded-full bg-slate-700 h-10 w-10"></div>
                     <div className="flex-1 space-y-6 py-1">
@@ -126,8 +136,12 @@ const ProductCategories = ({ countryMeta, handleCodeChange }) => {
                   </div>
                 </div>
               ))
-          : categories.map((info) => (
-              <Categories category={info} onClick={handleCodeChange} />
+          : categories.map((info, index) => (
+              <Categories
+                key={index}
+                category={info}
+                onClick={handleCodeChange}
+              />
             ))}
       </div>
       <div className="w-full">
@@ -151,12 +165,19 @@ export default function Products({
   })
 
   return (
-    <div className="h-full bg-white text-black font-lexEnd container mx-auto px-4 py-4">
-      {!countryMeta.code && <ProductsPromo />}
-      <ProductCategories
-        countryMeta={countryMeta}
-        handleCodeChange={(code: string) => setCountryMeta({ code })}
-      />
-    </div>
+    <main className="light">
+      <div className="h-20">
+        {/* Navbar  */}
+        <Navbar activePage={'Products'} />
+      </div>
+      <div className="h-full dark:bg-primary-dark bg-white text-black font-lexEnd container mx-auto px-4 py-4">
+        {!countryMeta.code && <ProductsPromo />}
+        <ProductCategories
+          countryMeta={countryMeta}
+          handleCodeChange={(code: string) => setCountryMeta({ code })}
+        />
+      </div>
+      <Footer />
+    </main>
   )
 }
--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -63,7 +63,16 @@ function App() {
               xmlns="http://www.w3.org/2000/svg"
               height="1em"
               viewBox="0 0 512 512"
-              width={"0.7rem"}
+              width={'0.7rem'}
+              className={`dark:${
+                slideIndex === currentIndex
+                  ? 'fill-blue-800'
+                  : 'fill-primary-light'
+              } ${
+                slideIndex === currentIndex
+                  ? 'fill-blue-800'
+                  : 'fill-neutral-800'
+              }`}
             >
               <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
             </svg>
